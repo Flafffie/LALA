2df58c30621f8fc7f60b410d4ae2a1a3c8d0bc39
irina <irina@DESKTOP-CQPLRA7>
2023-12-25 13:39:04 +0300

    Добавьте файлы проекта.

---
+++ LALA/MainViewModel.cs
@@ -0,0 +1,66 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LALA
+{
+    public class MainViewModel : ReactiveObject
+    {
+        private ObservableCollection<Student> _students;
+        public ObservableCollection<Student> Students
+        {
+            get => _students;
+            set => this.RaiseAndSetIfChanged(ref _students, value);
+        }
+
+        private string _newStudentName;
+        public string NewStudentName
+        {
+            get => _newStudentName;
+            set => this.RaiseAndSetIfChanged(ref _newStudentName, value);
+        }
+
+        private double _newStudentGrade;
+        public double NewStudentGrade
+        {
+            get => _newStudentGrade;
+            set => this.RaiseAndSetIfChanged(ref _newStudentGrade, value);
+        }
+
+        public ReactiveCommand<Unit, Unit> AddStudentCommand { get; }
+
+        public MainViewModel()
+        {
+            Students = new ObservableCollection<Student>();
+
+            AddStudentCommand = ReactiveCommand.Create(AddStudent);
+        }
+
+        private void AddStudent()
+        {
+            var newStudent = new Student
+            {
+                Name = NewStudentName,
+                Grade = NewStudentGrade
+            };
+
+            using (var context = new StudentDbContext())
+            {
+                context.Students.Add(newStudent);
+                context.SaveChanges();
+            }
+
+            Students.Add(newStudent);
+
+            // Очистка полей после добавления
+            NewStudentName = string.Empty;
+            NewStudentGrade = 0.0;
+        }
+    }
+
+}
---
+++ LALA/StudentDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LALA
+{
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("your_connection_string");
+        }
+    }
+
+}
